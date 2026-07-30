cd2b55926781996cf14c654076236a00685c25f2
yutowa15-cpu <yuto.wa15@gmail.com>
2026-07-30 13:58:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function shuffle(){
+
+    // 入力した人数を取得  people探す
+    let people = document.getElementById("people").value;  
+
+    let numbers = [];　　　// numberという箱を作る
+
+    let result = "";
+
+    // 1番〜人数分を作成
+    for(let i = 1; i <= people; i++){
+        numbers.push(i + "番");
+    }
+
+    // ランダムに並び替え
+    numbers.sort(() => Math.random() - 0.5);
+
+   //＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＝表形式に表示＝＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
+    result += "<table><tr>";
+
+    for(let i = 0; i <people; i++){
+        result +="<td>"+numbers[i]+ "</td>";
+
+        if ((i + 1) % 5 == 0)  {
+            result += "</tr>"+"<tr>";
+        }
+
+    }
+    result +=  "<tr></table>"
+
+//＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
+    // 結果表示
+
+    
+    document.getElementById("result").innerHTML = result;
+
+    
+}
